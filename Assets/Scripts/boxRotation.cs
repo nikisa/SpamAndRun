b1716dbe3268167a9c00937fe2db8858e0525c38
nikisa <nikisa97@gmail.com>
2019-03-15 13:47:25 +0100

    no message

--- Assets/Scripts/boxRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class boxRotation : MonoBehaviour {
-    void Update() {
-            transform.Rotate(new Vector3(0, 0, 10) * (Time.deltaTime));
-        }
-    }
